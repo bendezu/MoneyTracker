1d0644d4aadf3218592be7e3f3c252b69c618926
azinakov <ab.zinakov@yandex.ru>
2018-07-20 21:04:52 +0300

    update BaseActivity and MvpView

--- app/src/main/java/com/rygital/moneytracker/ui/base/MvpView.kt
+++ app/src/main/java/com/rygital/moneytracker/ui/base/MvpView.kt
@@ -1,4 +1,8 @@
 package com.rygital.moneytracker.ui.base
 
+import android.support.annotation.StringRes
+
 interface MvpView {
+    fun showMessage(@StringRes resId: Int)
+    fun showMessage(message: String)
 }
